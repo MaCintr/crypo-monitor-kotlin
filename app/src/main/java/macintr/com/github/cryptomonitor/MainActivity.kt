f318ddde7d43a375133aa8a33191d0455bfa6611
Marcelo <129231889+MaCintr@users.noreply.github.com>
2024-09-23 22:20:02 -0300

    Adding files

---
+++ app/src/main/java/macintr/com/github/cryptomonitor/MainActivity.kt
@@ -0,0 +1,13 @@
+package macintr.com.github.cryptomonitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
